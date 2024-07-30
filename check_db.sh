b639535885cfb40786714a79525ee91686bad32d
stefgrigo4 <stefan.grigorescu1@publicissapient.com>
2024-07-30 17:29:40 +0300

    on closed pr

--- check_db.sh
+++ check_db.sh
@@ -1,34 +1,38 @@
-#!/bin/bash
+#!/bin/sh
 
 # Configuration
-CONTAINER_NAME="test_database_container"
 MYSQL_USER="root"
 MYSQL_PORT="3306"
 MYSQL_PASSWORD="${TEST_DB_PASSWORD}"
 MYSQL_DATABASE="${TEST_DB}"
-MYSQL_TABLE="words_db"
+MYSQL_TABLE="words"
 MYSQL_COLUMN="word"
-
-
-# Function to check if the MySQL container is ready
-function wait_for_mysql() {
-  echo "Waiting for MySQL container to be ready..."
-  while true; do
-    if docker exec "$CONTAINER_NAME" mysqladmin ping -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent; then
-      echo "MySQL is up and running!"
-      break
-    else
-      echo "MySQL is not ready yet. Retrying in 5 seconds..."
-      sleep 5
-    fi
-  done
-}
+MYSQL_HOST="test_database_container"
 
 function query_database() {
-  echo "Querying the database for the necessary value..."
-  QUERY="SELECT * FROM $MYSQL_TABLE WHERE $MYSQL_COLUMN LIKE 'Test' OR $MYSQL_COLUMN LIKE 'test';"
-  docker exec "$CONTAINER_NAME" mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" -e "$QUERY"
+  echo "Querying the database for the specific value..."
+  QUERY_RESULT=$(mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -D "$MYSQL_DATABASE" -se "SELECT word FROM $MYSQL_TABLE where word='Test'")
+
+  if [ "$QUERY_RESULT" == "Test" ]; then
+    echo "Query result(s):"
+    echo "$QUERY_RESULT"
+    return 0
+  else      
+    echo "No matching records found."
+    return 1
+  fi
 }
 
-wait_for_mysql
-query_database
+# Loop to check the database every 5 seconds for up to 60 seconds
+for ((i=0; i<12; i++)); do
+  query_database
+  if [ $? -eq 0 ]; then
+    echo "Success: Found the matching record."
+    exit 0
+  fi
+  echo "Retrying in 5 seconds..."
+  sleep 5
+done
+
+echo "Failed: Could not find the matching record after 60 seconds."
+exit 1
